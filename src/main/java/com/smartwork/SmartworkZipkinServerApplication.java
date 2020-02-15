85e98413bf80dec0b6b2e239672c9f1931e74fed
Hank.huang <Hank.huang@shdbr.com>
2020-02-15 15:27:10 +0800

    'zipkin链路监控'

---
+++ src/main/java/com/smartwork/SmartworkZipkinServerApplication.java
@@ -0,0 +1,17 @@
+package com.smartwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class SmartworkZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartworkZipkinServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartwork/smartwork_zipkin_server/SmartworkZipkinServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.smartwork.smartwork_zipkin_server;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartworkZipkinServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
